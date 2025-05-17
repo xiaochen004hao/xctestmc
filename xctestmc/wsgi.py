ade526b803e3b7fd238aec305d39c994f03e14ff
xiaochen004hao <xiaochen004hao@163.com>
2025-05-17 11:53:25 +0800

    更新

--- xctestmc/wsgi.py
+++ xctestmc/wsgi.py
@@ -1,16 +1,23 @@
-"""
-WSGI config for xctestmc project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/5.1/howto/deployment/wsgi/
-"""
+# This file contains the WSGI configuration required to serve up your
+# web application at http://xcmctest.pythonanywhere.com/
+# It works by setting the variable 'application' to a WSGI handler of some
+# description.
+#
+# The below has been auto-generated for your Django project
 
 import os
+import sys
 
-from django.core.wsgi import get_wsgi_application
+# add your project directory to the sys.path
+project_home = '/home/xcmctest/xctestmc'
+if project_home not in sys.path:
+    sys.path.insert(0, project_home)
 
+# set environment variable to tell django where your settings.py is
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'xctestmc.settings')
 
+
+# serve django via WSGI
+from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
+
